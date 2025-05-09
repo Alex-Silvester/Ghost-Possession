3bbd0237d0be054e68837e009668fce39614dee0
Alex S <Alexander2.Silvester@live.uwe.ac.uk>
2025-05-09 13:13:24 +0100

    updated gitignore to stop updating out folder

--- out/build/x64-debug/_deps/flac-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alexs/Games_Tech/2nd year/various cpp/Ghost Possession/out/build/x64-debug/_deps/flac-src
-# Build directory: C:/Users/alexs/Games_Tech/2nd year/various cpp/Ghost Possession/out/build/x64-debug/_deps/flac-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("microbench")
--- out/build/x64-debug/_deps/flac-build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alexs/Games_Tech/2nd year/various cpp/Ghost Possession/out/build/x64-debug/_deps/flac-src/src
-# Build directory: C:/Users/alexs/Games_Tech/2nd year/various cpp/Ghost Possession/out/build/x64-debug/_deps/flac-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libFLAC")
